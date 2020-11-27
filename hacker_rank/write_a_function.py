6655f90a7dfbbcbaaf52aa430f6f7a773dd6fb38
nazmul199512 <nazmul199512@gmail.com>
2020-11-27 15:07:12 +0600

    print dunction solved

---
+++ hacker_rank/write_a_function.py
@@ -0,0 +1,11 @@
+def leaf_year(n):
+    if n % 400 == 0:
+        return True
+    if n % 100 == 0:
+        return False
+    if n % 4 == 0:
+        return True
+    return False
+
+
+print(leaf_year(int(input())))
